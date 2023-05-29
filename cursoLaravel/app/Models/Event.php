18dfd5408cde84953c2d1fee47669c0e9151474e
Dimas Ferreira <dimaslimaferreira@gmail.com>
2023-05-29 20:33:56 -0300

    update

--- cursoLaravel/app/Models/Event.php
+++ cursoLaravel/app/Models/Event.php
@@ -9,11 +9,19 @@ class Event extends Model
 {
     use HasFactory;
 
-
     protected $casts = [
         'items' => 'array'
     ];
 
     protected $dates = ['date'];
-}
 
+    protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function users() {
+        return $this->belongsToMany('App\Models\User');
+    }
+}
