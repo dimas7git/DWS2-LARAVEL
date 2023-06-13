09bf541608c781ac0d1d4cc6724dfbd3a9d94585
Dimas Ferreira <dimaslimaferreira@gmail.com>
2023-06-13 19:15:56 -0300

    update

--- cadastroFunc/resources/views/product.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-@section ('title', 'Dimas')
-@section ('content')   
-
-@if($id != null)
-<p>Exibindo produtos id: {{$id}}</p>
-@endif
-@endsection
--- cadastroFunc/resources/views/products.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-@section ('title', 'Dimas')
-@section ('content')   
-
-
-<h1>tela de produto</h1>
-@if($busca != '')
-<p>O usuario esta boscando por : {{$busca}}</p>
-@endif
